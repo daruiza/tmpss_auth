a929c4b5d98954ce920977f5e27e2490e4f8a695
daruiza <daruiza@gmail.com>
2024-07-04 16:29:36 -0500

    exepciones OK

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,16 +7,53 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller
 {
+    private $name = 'name';
+    private $email = 'email';
+    private $password = 'password';
     
     public function login(Request $request)
     {
+
+        $rules = [
+            $this->email    => 'required|string|max:128|email',
+            $this->password => 'required|string',
+        ];
+
+        try {
+
+            $credentials = request([$this->name, $this->email, $this->password]);
+            $validator = Validator::make($request->all(), $rules);
+            if (!Auth::attempt($credentials) || $validator->fails()) {
+                throw (new ValidationException($validator));
+            }
+
+        } 
+        catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Credenciales mal suministradas', 
+                'error' => $e->validator->errors()->messages()],
+                403);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Credenciales no autorizadas', 
+                'error' => $e],
+                403);
+        }
+
         return response()->json([
             'data'=>'login',
             // '$request'=>$request,
             'user'=>Auth::user(),
-            'check'=>Auth::check()
+            'check'=>Auth::check(),
+            '$credentials'=>$credentials,
+            '$validator'=>$validator,
+            'attempt'=>Auth::attempt($credentials)
         ]);
     }
 
